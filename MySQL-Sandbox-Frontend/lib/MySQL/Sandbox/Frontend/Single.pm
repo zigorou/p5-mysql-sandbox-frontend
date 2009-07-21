0a56bcd4ff9c6af45db7438d99c065379f79a014
yamaguchi.toru <zigorou@cpan.org>
2009-07-21 12:50:48 +0900

    implement parse, dbh, dbh_args ...

--- MySQL-Sandbox-Frontend/lib/MySQL/Sandbox/Frontend/Single.pm
+++ MySQL-Sandbox-Frontend/lib/MySQL/Sandbox/Frontend/Single.pm
@@ -7,108 +7,146 @@ our $VERSION = '0.01';
 
 use base qw(Class::Accessor::Fast);
 
-__PACKAGE__->mk_accessors(qw/
-				upper_directory
-				sandbox_directory
-				sandbox_port
-				db_user
-				db_password
-			    /);
+__PACKAGE__->mk_accessors(
+    qw/
+      upper_directory
+      sandbox_directory
+      sandbox_port
+      db_user
+      db_password
+      /
+);
 
 use Carp;
 use Data::Dumper;
-use File::Spec qw(catdir);
+use DBI;
+use File::Spec;
 use MySQL::Sandbox qw(
-			 is_a_sandbox
-			 get_sandbox_params
-			 is_sandbox_running
-		 );
+  is_a_sandbox
+  get_sandbox_params
+  is_sandbox_running
+);
 use IPC::Cmd qw(can_run run);
+use Sys::HostIP;
 
 use MySQL::Sandbox::Frontend;
 
 sub new {
-    my ($class, $args) = @_;
+    my ( $class, $args ) = @_;
 
     $args ||= +{};
     $args = +{
-	upper_directory => catdir($ENV{HOME}, 'sandboxes'),
-	db_user		=> 'msandbox',
-	db_password	=> 'msandbox',
-	%$args,
+        upper_directory => File::Spec->catdir( $ENV{HOME}, 'sandboxes' ),
+        db_user         => 'msandbox',
+        db_password     => 'msandbox',
+        %$args,
     };
-    
+
     $class->SUPER::new($args);
 }
 
 sub create {
-    my ($self, $version, $opts) = @_;
+    my ( $self, $version, $opts ) = @_;
 
     $opts ||= +{};
     $opts = +{
-	force => 0,
-	check_port => 1,
-	%$opts,
-	no_confirm => 1,
+        force      => 0,
+        check_port => 1,
+        %$opts,
+        no_confirm => 1,
     };
 
-    croak(q|Cannot run make_sandbox command|) unless(can_run('make_sandbox'));
+    croak(q|Cannot run make_sandbox command|)
+      unless ( can_run('make_sandbox') );
 
-    my @cmd = ('make_sandbox', $version);
-    
-    for my $raw_opt (grep { $opts->{$_} } qw/force check_port no_confirm/) {
-	push(@cmd, '--' . $raw_opt);
+    my @cmd = ( 'make_sandbox', $version );
+
+    for my $raw_opt ( grep { $opts->{$_} } qw/force check_port no_confirm/ ) {
+        push( @cmd, '--' . $raw_opt );
     }
 
-    my ($success, $err_code, $full_buf, $stdout, $stderr) = run(
-	command => \@cmd,
-	verbose => $MySQL::Sandbox::Frontend::DEBUG,
-	timeout => $MySQL::Sandbox::Frontend::TIMEOUT,
+    push( @cmd, '--sandbox_directory', $self->sandbox_directory ) if ($self->sandbox_directory);
+
+    my ( $success, $err_code, $full_buf, $stdout, $stderr ) = run(
+        command => \@cmd,
+        verbose => $MySQL::Sandbox::Frontend::DEBUG,
+        timeout => $MySQL::Sandbox::Frontend::TIMEOUT,
     );
 
     unless ($success) {
-	croak($err_code);
+        croak($err_code);
     }
 
     return $self->parse($stdout);
 }
 
 sub parse {
-    my ($self, $stdout) = @_;
-    my $is_installed = $stdout->[-1] =~ m|^Your sandbox server was installed in (.*)\n$|;
+    my ( $self, $stdout ) = @_;
+    my $is_installed =
+      $stdout->[-1] =~ m|^Your sandbox server was installed in (.*)\n$|;
 
     if ($is_installed) {
-	my $config_text  = $stdout->[2];
-	my %config = $config_text =~ m|^([\w_]+)\s*=\s*([^=\s]*)\n|mg;
-	my @fields = qw(db_user db_password upper_directory sandbox_directory sandbox_port);
+        my $config_text = $stdout->[1];
+        my %config = $config_text =~ m|^([\w_]+)\s*=\s*([^=\s]*)\n|mg;
+        my @fields =
+          qw(db_user db_password upper_directory sandbox_directory sandbox_port);
 
-	for (@fields) {
-	    $self->$_($config{$_});
-	}
+        for (@fields) {
+            $self->$_( $config{$_} );
+        }
     }
 
     return $is_installed;
 }
 
+sub dbh_args {
+    my ($self, $database) = @_;
+
+    my %dsn = (
+	host => Sys::HostIP->ip,
+	($database) ? (db => $database) : (),
+	port => $self->sandbox_port,
+    );
+
+    return +{
+	dsn => 'dbi:mysql:' . join( ';' => map { $_ . '=' . $dsn{$_} } keys %dsn ),
+	user => $self->db_user,
+	password => $self->db_password,
+    };
+}
+
+sub dbh {
+    my ($self, $database, $args) = @_;
+    $args ||= +{};
+    my $dbh_args = $self->dbh_args($database);
+    my $dbh = DBI->connect(@$dbh_args{qw/dsn user password/}, $args);
+    return $dbh;
+}
+
 sub sandbox_abs_directory {
     my $self = shift;
 
-    if (-d $self->upper_directory && $self->sandbox_directory) {
-	return cat_dir($self->upper_directory, $self->sandbox_directory);
+    if ( -d $self->upper_directory && $self->sandbox_directory ) {
+        return File::Spec->catdir( $self->upper_directory, $self->sandbox_directory );
     }
     else {
-	return "";
+        return "";
     }
 }
 
 sub is_exists {
-    my $self = shift;
-
-    is_a_sandbox();
+    my $self    = shift;
+    my $abs_dir = $self->sandbox_abs_directory;
+    is_a_sandbox($abs_dir) ? 1 : 0;
 }
 
 sub is_running {
     my $self = shift;
+    my $ret;
+    eval {
+	$ret = is_sandbox_running($self->sandbox_abs_directory);
+    };
+    $ret ? 1 : 0;
 }
 
 1;
